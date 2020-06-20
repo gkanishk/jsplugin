d0db0f38c3384056f0608dcf65d46ac22ec3c178
Kanishk Gupta <kanishkgupta1234@gmail.com>
2020-06-20 06:44:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var head = document.getElementsByTagName('HEAD')[0];  
+var link = document.createElement('link'); 
+link.rel = 'stylesheet';  
+link.type = 'text/css'; 
+link.href = 'css/style.css';  
+head.appendChild(link); 
+
+var script = document.getElementById('emb')
+var data= script.getAttribute("data");
+console.log(data+" hui");
+document.write(`
+<div class="card">
+<div id="dimg"><img src="${data}/favicon.ico" id="uimg"/></div>
+<div class="container">
+<a href=${data} target="_blank" rel="noreferrer noopener" class=button1>Visit</a>
+<h4>${data}</h4>
+</div>
+</div>`);
